dc256b4557e395e77a9e9da664a63a40f4cf3a7f
arturican <arturican@gmail.com>
2023-11-17 23:00:52 +0300

    deploy

--- src/HW/1/HW1.tsx
+++ src/HW/1/HW1.tsx
@@ -31,7 +31,6 @@ export const HW1 = () => {
 		},
 		{id: 10, name: 'Emily', age: 55, address: {street: '765 Aspen Blvd', city: 'Los Angeles'}},
 	];
-
 	return (
 		<div id={'hw01'}>
 			<UserList users={users} />
